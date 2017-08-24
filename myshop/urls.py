aaf42cbb9ec91697b78aeeb252b30f0a7b5a7acb
damvantai <tai94bn@gmail.com>
2017-08-24 10:03:58 +0700

    include urls for the shop application under a custom namespace named 'shop'

--- myshop/urls.py
+++ myshop/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from shop import views
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
+    url(r'^$', views.product_list, name='product_list'),
+    url(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
 ]
