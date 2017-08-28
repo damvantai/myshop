7ba303da9ac476eaadb8ef98ae26ccaf527dd192
damvantai <tai94bn@gmail.com>
2017-08-28 08:23:48 +0700

    add url paypal above shop

--- myshop/settings.py
+++ myshop/settings.py
@@ -128,3 +128,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+PAYPAL_RECEIVER_EMAIL = 'tai94bn@gmail.com'
+PAYPAL_TEST = True
