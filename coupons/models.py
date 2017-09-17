98b735c1f10d8bebae58c553e9315f165fe9242f
damvantai <tai94bn@gmail.com>
2017-09-17 21:26:02 +0700

    repair validators in discount MaxValueValidator

--- coupons/models.py
+++ coupons/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxLengthValidator
+from django.core.validators import MinValueValidator, MaxLengthValidator, MaxValueValidator
+
 
 # Create your models here.
 class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
     valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
-    discount = models.IntegerField(validators=[MinValueValidator(0), MaxLengthValidator(100)])
+    discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     active = models.BooleanField()
 
     def __str__(self):
